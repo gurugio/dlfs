a433d04bec4c78f248941c228c52e05c14e93db9
Gioh Kim <gi-oh.kim@cloud.ionos.com>
2019-10-18 17:53:23 +0200

    two layer network

--- chap4.py
+++ chap4.py
@@ -44,19 +44,39 @@ def numerical_diff(f, x):
     h = 1e-4
     return (f(x+h) - f(x-h)) / (2 * h)
 
-def numerical_gradient(f, x):
-    h = 1e-4
-    grad = np.zeros_like(x)
+# def numerical_gradient(f, x):
+#     h = 1e-4
+#     grad = np.zeros_like(x)
 
-    for idx in range(x.size):
+#     for idx in range(x.size):
+#         tmp_val = x[idx]
+#         x[idx] = tmp_val + h
+#         fxh1 = f(x)
+
+#         x[idx] = tmp_val - h
+#         fxh2 = f(x)
+#         grad[idx] = (fxh1 - fxh2) / (2 * h)
+#         x[idx] = tmp_val
+#     return grad
+
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
         tmp_val = x[idx]
         x[idx] = tmp_val + h
-        fxh1 = f(x)
-
-        x[idx] = tmp_val - h
-        fxh2 = f(x)
-        grad[idx] = (fxh1 - fxh2) / (2 * h)
-        x[idx] = tmp_val
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val # 値を元に戻す
+        it.iternext()   
+        
     return grad
 
 def gradient_descent(f, init_x, lr = 0.01, step_num = 100):
@@ -70,7 +90,83 @@ def gradient_descent(f, init_x, lr = 0.01, step_num = 100):
 def function_2(x):
     return x[0] ** 2 + x[1] ** 2
 
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+        return loss
+
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+
+    # x: input data, t: answer label
+    def loss(self, x, t):
+        y = self.predict(x)
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis = 1)
+        t = np.argmax(t, axis = 1)
+
+        print(y)
+        print(t)
+        
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        return grads
+    
+
 if __name__ == '__main__':
+    net = TwoLayerNet(input_size = 784, hidden_size = 100, output_size = 10)
+    print(net.params['W1'].shape)
+    print(net.params['b1'].shape)
+    print(net.params['W2'].shape)
+    print(net.params['b2'].shape)
+
+    x = np.random.rand(100, 784)
+    t = np.random.rand(100, 10)
+    grads = net.numerical_gradient(x, t)
+    print(grads['W1'].shape)
+    print(grads['b1'].shape)
+    print(grads['W2'].shape)
+    print(grads['b2'].shape)
+    
+
     # (x_train, t_train), (x_test, t_test) = \
     #     load_mnist(normalize = True, one_hot_label = True)
 
@@ -86,6 +182,16 @@ if __name__ == '__main__':
     # t_batch = t_train[batch_mask]
     # print(t_batch.shape)
 
-    init_x = np.array([-3.0, 4.0])
-    print(gradient_descent(function_2, init_x = init_x, lr = 0.01, step_num = 1000))
-    
+    # init_x = np.array([-3.0, 4.0])
+    # print(gradient_descent(function_2, init_x = init_x, lr = 0.01, step_num = 1000))
+
+    # net = simpleNet()
+    # print(net.W)
+    # x = np.array([0.6, 0.9])
+    # p = net.predict(x)
+    # print(p)
+    # np.argmax(p)
+    # t = np.array([0, 0, 1])
+    # print('initial loss:', net.loss(x, t))
+    # dW = numerical_gradient(lambda w: net.loss(x, t), net.W)
+    # print(dW)
