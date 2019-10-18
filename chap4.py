39f212dd49e3ed04b9f3e2b64da2817bd49e51da
Gioh Kim <gi-oh.kim@cloud.ionos.com>
2019-10-18 18:23:07 +0200

    finish chap4: image recognition with 2 layer network

--- chap4.py
+++ chap4.py
@@ -152,19 +152,45 @@ class TwoLayerNet:
     
 
 if __name__ == '__main__':
-    net = TwoLayerNet(input_size = 784, hidden_size = 100, output_size = 10)
-    print(net.params['W1'].shape)
-    print(net.params['b1'].shape)
-    print(net.params['W2'].shape)
-    print(net.params['b2'].shape)
+    (x_train, t_train), (x_test, t_test) = \
+        load_mnist(normalize = True, one_hot_label = True)
+    train_loss_list = []
+    iters_num = 100
+    train_size = x_train.shape[0]
+    batch_size = 100
+    learning_rate = 0.1
+    network = TwoLayerNet(input_size = x_train.shape[1],
+                          hidden_size = 50,
+                          output_size = t_train.shape[1])
 
-    x = np.random.rand(100, 784)
-    t = np.random.rand(100, 10)
-    grads = net.numerical_gradient(x, t)
-    print(grads['W1'].shape)
-    print(grads['b1'].shape)
-    print(grads['W2'].shape)
-    print(grads['b2'].shape)
+    for i in range(iters_num):
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+
+        grad = network.numerical_gradient(x_batch, t_batch)
+        for key in ('W1', 'b1', 'W2', 'b2'):
+            network.params[key] -= learning_rate * grad[key]
+
+        loss = network.loss(x_batch, t_batch)
+        train_loss_list.append(loss)
+
+    print(train_loss_list)
+    
+
+
+    # net = TwoLayerNet(input_size = 784, hidden_size = 100, output_size = 10)
+    # print(net.params['W1'].shape)
+    # print(net.params['b1'].shape)
+    # print(net.params['W2'].shape)
+    # print(net.params['b2'].shape)
+    # x = np.random.rand(100, 784)
+    # t = np.random.rand(100, 10)
+    # grads = net.numerical_gradient(x, t)
+    # print(grads['W1'].shape)
+    # print(grads['b1'].shape)
+    # print(grads['W2'].shape)
+    # print(grads['b2'].shape)
     
 
     # (x_train, t_train), (x_test, t_test) = \
